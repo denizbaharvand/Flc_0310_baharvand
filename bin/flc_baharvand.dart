9b0be0601ff74c434e04ec65ff87d954abed145a
denizbaharvand <deniz82baharvand@gmail.com>
2025-01-15 19:12:02 +0330

    initialcommit

---
+++ bin/flc_baharvand.dart
@@ -0,0 +1,5 @@
+import 'package:flc_baharvand/flc_baharvand.dart' as flc_baharvand;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flc_baharvand.calculate()}!');
+}
---
+++ test/flc_baharvand_test.dart
@@ -0,0 +1,8 @@
+import 'package:flc_baharvand/flc_baharvand.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
